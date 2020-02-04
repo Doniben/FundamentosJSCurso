45f8a5e32d1d3f77a970a5751b53776846e511fd
Doniben <doniben@esperanto.co>
2020-02-04 17:18:44 -0500

    [FEATURE] base html that calls a javascript class

--- clase.js
+++ clase.js
@@ -1,8 +1,26 @@
 var nombre
 
-nombre = 'Sasha'
-apelli = 'Ala'
+nombre = 'SaSha'
+var apelli = 'Ala'
 var edad = 28
+var peso = 76
 
 console.log('Hola ' + nombre + ' ' + apelli)
 console.log('tengo ' + edad + " años")
+console.log('tengo ' + peso + ' kgs')
+
+
+Nom_may = nombre.toUpperCase()
+ape_low = apelli.toLowerCase()
+char = nombre.charAt(2)
+
+var lens = apelli.lenght
+
+console.log(Nom_may)
+console.log(ape_low)
+console.log(char)
+console.log(lens)
+
+var nombreCompleto = `${nombre} ${apelli.toUpperCase()}`
+
+let str1 = apelli.substr(1, 2)
